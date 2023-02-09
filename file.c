4f439f1859170eb5afe1a53a6adc07fe8923c89b
Sam <samuel.reuben@gmail.com>
2023-02-09 17:21:29 +0530

    Sam changing same file c to test pull request

--- file.c
+++ file.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
 int main(){
     printf("this is git practice");
+
+
+
+
+    int num;
+    printf("Sam's changes making a scanf enter a number : ");
+    scanf("%d",&num);
+    printf("--------------------------------------------------------");
+    printf("Sam's change number : %d\n",num);
     return 0;
 }
