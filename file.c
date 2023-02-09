0f3085bee7b060d2701cfbd9d2f3a82adc63c738
HanzalaKhamkar <sthka05@gmail.com>
2023-02-09 17:28:54 +0530

    changes made by hanzala

--- file.c
+++ file.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 int main(){
+ int x;
  printf("this sucks");  
  printf("this is git practice");
-    return 0;
+ scanf("%d",&x);
+ printf("enter the value of x : "); 
+   return 0;
 }
